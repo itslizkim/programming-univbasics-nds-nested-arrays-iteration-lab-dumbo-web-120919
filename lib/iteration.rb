139b9a480de440d74f509a71d656eea62bafaa3b
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-07 00:14:53 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -40,9 +40,9 @@ def total_even_pairs(src)
   row_index = 0
   while row_index < array_3.count do
     if (array_3[row_index][0] % 2) && (array_3[row_index][1] % 2)
-      total += (array_3[row_index][0] + array_3[row_index][1]
+      total += (array_3[row_index][0] + array_3[row_index][1])
     end
-    
+
     row_index += 1
   end
 
